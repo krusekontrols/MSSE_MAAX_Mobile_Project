67897f640336fbb890201f35810e4ff794f7ab8d
Adit007 <adit.dutt@gmail.com>
2013-05-01 07:45:56 -0500

    Create ItemPromotionsDetailsActivity.java

---
+++ src/edu/umn/itempro/ItemPromotionsDetailsActivity.java
@@ -0,0 +1,14 @@
+package edu.umn.itempro;
+
+import android.app.Activity;
+import android.widget.Button;
+import android.widget.EditText;
+
+/** A Class to display the promotions details available on the items
+ *
+ */
+
+public class ItemPromotionsDetailsActivity extends Activity {
+
+}
+
