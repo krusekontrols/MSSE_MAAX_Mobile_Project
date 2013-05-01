681f212852e7afc85a06264b941d79e112280c16
Adit007 <adit.dutt@gmail.com>
2013-05-01 07:45:10 -0500

    Create ItemListActivity.java

---
+++ src/edu/umn/itempro/ItemListActivity.java
@@ -0,0 +1,13 @@
+package edu.umn.itempro;
+
+import android.app.Activity;
+import android.widget.Button;
+import android.widget.EditText;
+
+/** A Class to display list of items
+ *
+ */
+
+public class ItemListActivity extends Activity {
+
+}
