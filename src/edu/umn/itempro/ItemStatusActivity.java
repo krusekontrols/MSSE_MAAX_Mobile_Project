18be624911a6eb09a954917249451175575a14b8
Adit007 <adit.dutt@gmail.com>
2013-05-01 07:45:34 -0500

    Create ItemStatusActivity.java

---
+++ src/edu/umn/itempro/ItemStatusActivity.java
@@ -0,0 +1,13 @@
+package edu.umn.itempro;
+
+import android.app.Activity;
+import android.widget.Button;
+import android.widget.EditText;
+
+/** A Class to display the status of the items
+ *
+ */
+
+public class ItemStatusActivity extends Activity {
+
+}
