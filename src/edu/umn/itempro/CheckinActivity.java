25614ff0247002879d50427743366fe0be6b220f
Adit007 <adit.dutt@gmail.com>
2013-05-01 07:36:03 -0500

    Create CheckinActivity.java

---
+++ src/edu/umn/itempro/CheckinActivity.java
@@ -0,0 +1,13 @@
+package edu.umn.itempro;
+
+import android.app.Activity;
+import android.widget.Button;
+import android.widget.EditText;
+
+/** A Class for User to Checkin a store
+*
+*/
+
+public class CheckinActivity extends Activity {
+
+}
