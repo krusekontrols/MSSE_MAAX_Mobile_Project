f355074f680e80526c20eba004be840edb463db3
Adit007 <adit.dutt@gmail.com>
2013-05-01 07:36:32 -0500

    Create PopupActivity.java

---
+++ src/edu/umn/itempro/PopupActivity.java
@@ -0,0 +1,13 @@
+package edu.umn.itempro;
+
+import android.app.Activity;
+import android.widget.Button;
+import android.widget.EditText;
+
+/** A Class for Popup screen
+*
+*/
+
+public class PopupActivity extends Activity {
+
+}
