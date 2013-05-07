8d5c4614524d96316b909c0241eed2a1fee37d68
Adit007 <adit.dutt@gmail.com>
2013-05-07 01:12:49 -0500

    checkIn functionality

--- src/edu/umn/itempro/CheckinActivity.java
+++ src/edu/umn/itempro/CheckinActivity.java
@@ -3,20 +3,51 @@ package edu.umn.itempro;
 import android.app.Activity;
 import android.os.Bundle;
 
-/** A Class for User to Checkin a store
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+/** A Class for User to Checkin to a particular store
+ * We are considering two stores: Best Buy, Target 
 *
 */
 
 public class CheckinActivity extends Activity {
 
+  Button b1,b2;
+
 	/** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.checkin);
+  @Override
+  public void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.checkin);
+
+    b1 = (Button) findViewById(R.id.button1);
+    b2 = (Button) findViewById(R.id.button2);
+
+    b1.setOnClickListener(new OnClickListener()
+    {
+    public void onClick(View v)
+    {
+    	Toast msg = Toast.makeText(getBaseContext(), "You are connected to Best Buy", Toast.LENGTH_LONG);                                                                 
+    	msg.show();
+    }                                
+    });
+
+    b2.setOnClickListener(new OnClickListener()
+    {
+    public void onClick(View v)
+    {
+    	Toast msg = Toast.makeText(getBaseContext(), "You are connected to Target", Toast.LENGTH_LONG);
+    	msg.show();
     }
-    
-    @Override
-    public void onBackPressed() {
-    }
+    });
+  }
+  
+  @Override
+  public void onBackPressed() {
+  }
+
 }
